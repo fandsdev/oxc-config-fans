4889b391bae7a619280e48233174d2fd35710526
Eduard Aksamitov <e@euaaaio.ru>
2026-05-05 19:08:32 +0300

    feat(react): add recommended rules

--- src/oxlint/configs/react.js
+++ src/oxlint/configs/react.js
@@ -4,5 +4,13 @@ export default defineConfig({
 	plugins: ['react', 'react-perf'],
 	rules: {
 		'react/react-in-jsx-scope': 'off',
+
+		// From eslint-plugin-react recommended
+		'react/display-name': 'error',
+		'react/no-unescaped-entities': 'error',
+		'react/require-render-return': 'error',
+
+		// From eslint-plugin-react-hooks recommended
+		'react/rules-of-hooks': 'error',
 	},
 })
